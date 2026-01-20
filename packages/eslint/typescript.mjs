abad59f66fab24b07c3f7001604e39811bbfa713
Shim Heungwoon <ajaxlab7@gmail.com>
2026-01-21 01:09:53 +0900

    chore: add eslint-plugin-prettier

--- packages/eslint/typescript.mjs
+++ packages/eslint/typescript.mjs
@@ -1,14 +1,27 @@
 import js from '@eslint/js'
-import prettier from 'eslint-config-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  prettier,
+  prettierConfig,
   {
     ignores: ['node_modules/**', 'dist/**', '.next/**', '.turbo/**'],
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': 'error',
+    },
   },
   {
     files: ['**/*.js', '**/*.cjs'],
